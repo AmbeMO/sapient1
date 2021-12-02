28f8ab5d440333bb2213fe94b587dab4501fd5d2
milanamarkovska <milamo228322@gmail.com>
2021-12-03 01:27:01 +0200

    20% yeee ( end general visual and valid for create/change page )

--- AngularApi/angular-sapient/src/app/app.module.ts
+++ AngularApi/angular-sapient/src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
+import {OrgListComponent} from "./orgList/orgList.component";
+import { CreateChangeOrgComponent } from './create-change-org/create-change-org.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { ChangeRoleComponent } from './change-role/change-role.component';
+import { NavComponent } from './nav/nav.component';
+import { LinkBarComponent } from './link-bar/link-bar.component';
+import {AppRoutingModule} from "./app.routing.module";
+import {MainLayoutComponent} from "./shared/components/main-layout.component";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrgListComponent,
+    CreateChangeOrgComponent,
+    SideBarComponent,
+    ChangeRoleComponent,
+    NavComponent,
+    LinkBarComponent,
+    MainLayoutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
